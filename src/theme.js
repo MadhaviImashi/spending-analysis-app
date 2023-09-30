baccba7f37f77412598c8d834f3f80829f1a1628
madhaviimashi <madhaviimashi@gmail.com>
2023-09-30 09:07:09 +0530

    side nav bar & header completed

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "",
+            light: ""
+        },
+        secondary: {
+            main: "",
+            light: "",
+        },
+        other: {
+            text: "",
+            error: "",
+        }
+    }
+})
